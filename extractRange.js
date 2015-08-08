3b1aeb5ae08fd5eb137d3d396828fcb677f91304
yanninho <y4nn5m@gmail.com>
2015-08-08 07:15:39 +0200

    add error

--- extractRange.js
+++ extractRange.js
@@ -3,8 +3,24 @@
 * Extract filters from request
 * Example : /test?range=10-20 give req.happyRest.filters = {"offset" : "10", "limit" : "20" }
 **/
+var txtErrorConf = 'Server middleware are not well configured';
+
+var addAcceptRange = function(req, res) {
+    if (!req.happyRest) res.status(500).send({reason : txtErrorConf});
+    if (req.resourceName && req.maxResult) {
+      res.setHeader('Accept-Range', req.resourceName + ' ' + req.maxResult);    
+    }        
+}
+
+var addContentRange = function(req, res) {
+  if (!req.happyRest) res.status(500).send({reason : txtErrorConf});
+  if (req.happyRest.range && req.count) {
+    res.setHeader('Content-Range', req.happyRest.range.offset + '-' + req.happyRest.range.limit + '/'+ req.count );
+  }      
+}
 
 module.exports = {
+
   extractRange: function(req, res, next) {
     var maxResult = req.maxResult || 50;
     if (!req.happyRest) req.happyRest = {};
@@ -23,7 +39,8 @@ module.exports = {
   }
 
   if (rangeArray[1] > maxResult) {
-    return next({status:400, reason : 'Requested range not allowed'});
+    addAcceptRange(req, res);
+    res.status(400).send({reason : 'Requested range not allowed'});
   }
   req.happyRest.range = {
     offset : rangeArray[0],
@@ -34,15 +51,18 @@ module.exports = {
   },
   
   setHeader: function(req, res, next) {        
-      if (!req.happyRest) return next(new Error({status:500,reason : 'Server middleware are not well configured'}));
-
-      if (req.resourceName && req.maxResult) {
-        res.setHeader('Accept-Range', req.resourceName + ' ' + req.maxResult);    
-      }
-
-      if (req.happyRest.range && req.count) {
-        res.setHeader('Content-Range', req.happyRest.range.offset + '-' + req.happyRest.range.limit + '/'+ req.count );
-      }
+    addAcceptRange(req, res);
+    addContentRange(req, res);
     next();    
+  },
+
+  addAcceptRange: function(req, res, next) {
+    addAcceptRange(req, res);
+    next();
+  },
+
+  addContentRange: function(req, res, next) {
+    addContentRange(req, res);
+    next();
   }
 };
